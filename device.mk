cef57a5f5d6ece9eb5c0cb555be89b158f4b3528
Hatsune71 <evraisecheat2@gmail.com>
2020-07-21 19:10:49 +0700

    ysl: Build Display Service 1.0

--- device.mk
+++ device.mk
@@ -33,6 +33,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Display
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0
+
 # Properties
 -include device/xiaomi/ysl/prop.mk
 
