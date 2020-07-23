e0baeeb42040535e9248584acface4e2dbf2255d
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2020-07-23 22:49:58 +0200

    bringup of SSoS (1/2)

--- aosp_ysl.mk
+++ aosp_ysl.mk
@@ -21,16 +21,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ysl device
 $(call inherit-product, device/xiaomi/ysl/device.mk)
 
-# Inherit some common Corvus OS stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+# Inherit some common ShapeShiftOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Corvus OS Official Device Tree
+# Inherit some common ShapeShiftOS stuff.
 TARGET_BOOT_ANIMATION_RES := 720
-DU_BUILD_TYPE := OFFICIAL
+TARGET_GAPPS_ARCH := arm64
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+export LAWNCHAIR_OPTOUT := true
+export TARGET_PIXEL_LWP := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
-PRODUCT_NAME := du_ysl
+PRODUCT_NAME := aosp_ysl
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi S2
@@ -43,4 +48,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
