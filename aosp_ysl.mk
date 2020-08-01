f709a580ff472afc48559e678ca01ecddc24be74
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2020-08-01 14:47:36 +0200

    revert back the fp

--- aosp_ysl.mk
+++ aosp_ysl.mk
@@ -48,4 +48,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
